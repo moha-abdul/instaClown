ae4d82d3143ea2860460107f675c2da89d09daad
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-10-03 18:30:33 +0300

    Create homepage

---
+++ gram/urls.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns=[
+    url('^$',views.hello,name = 'hello'),
+]
