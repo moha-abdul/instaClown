59d30737c0b06f8469713456c4f641ae4c7e50f6
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-10-05 12:38:21 +0300

    fix email in environment

--- insta/settings.py
+++ insta/settings.py
@@ -135,6 +135,6 @@ django_heroku.settings(locals())
 
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT')
